74ed4401afe4f4ab716b3810d719e51af675b29a
gbfish <equalbean@gmail.com>
2015-03-11 13:19:45 -0700

    Initial Commit

---
+++ Duang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Duang
+//
+//  Created by David Yu on 15/3/11.
+//  Copyright (c) 2015年 David Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
