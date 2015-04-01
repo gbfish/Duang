b95343f7def09ece6b98e6bf90e41648f3a37895
gbfish <equalbean@gmail.com>
2015-03-31 18:10:26 -0700

    ok

--- Duang/DuangTableCellImageMutable.swift
+++ Duang/DuangTableCellImageMutable.swift
@@ -47,84 +47,88 @@ class DuangTableCellImageMutable: UITableViewCell
 
     }
     
+    var hasAddImageViews = false
+    
+    func addImageViews() {
+        if !hasAddImageViews {
+            imageView1.contentMode = UIViewContentMode.ScaleAspectFill
+            imageView2.contentMode = UIViewContentMode.ScaleAspectFill
+            imageView3.contentMode = UIViewContentMode.ScaleAspectFill
+            imageView4.contentMode = UIViewContentMode.ScaleAspectFill
+            imageView5.contentMode = UIViewContentMode.ScaleAspectFill
+            imageView1.clipsToBounds = true
+            imageView2.clipsToBounds = true
+            imageView3.clipsToBounds = true
+            imageView4.clipsToBounds = true
+            imageView5.clipsToBounds = true
+            addSubview(imageView1)
+            addSubview(imageView2)
+            addSubview(imageView3)
+            addSubview(imageView4)
+            addSubview(imageView5)
+            hasAddImageViews = true
+        }
+    }
+    
+    
     var photos: PFRelation? {
         didSet {
+            addImageViews()
+            imageView1.hidden = true
+            imageView2.hidden = true
+            imageView3.hidden = true
+            imageView4.hidden = true
+            imageView5.hidden = true
+            
+            let spacing: CGFloat = 4.0
+            
             if let relation = photos {
                 relation.query().findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
                     if error == nil {
-                        switch objects.count {
+                        let photos = objects as [PFObject]
+                        switch photos.count {
                         case 1:
-                            self.imageView1 = UIImageView(frame: self.bounds)
+                            self.loadImage(self.imageView1, rect: self.bounds, file: self.getFile(photos, index: 0))
                             
-                            self.imageView2 = UIImageView()
-                            self.imageView3 = UIImageView()
-                            self.imageView4 = UIImageView()
-                            self.imageView5 = UIImageView()
+                        case 2:
+                            let rect1 = CGRectMake(0.0, 0.0, (self.bounds.width - spacing) / 2, self.bounds.height)
+                            let rect2 = CGRectMake(rect1.width + spacing , 0.0, rect1.width, self.bounds.height)
                             
-                            self.addSubview(self.imageView1)
-                            self.addSubview(self.imageView2)
-                            self.addSubview(self.imageView3)
-                            self.addSubview(self.imageView4)
-                            self.addSubview(self.imageView5)
+                            self.loadImage(self.imageView1, rect: rect1, file: self.getFile(photos, index: 0))
+                            self.loadImage(self.imageView2, rect: rect2, file: self.getFile(photos, index: 1))
                             
-                            let photos = objects as [PFObject]
-                            let photo: PFObject = objects[0] as PFObject
-                            let imageFile: PFFile = photo[TablePhoto.Image] as PFFile
+                        case 3:
+                            let rect1 = CGRectMake(0.0, 0.0, (self.bounds.width - spacing) / 2, self.bounds.height)
+                            let rect2 = CGRectMake(rect1.width + spacing , 0.0, rect1.width, (self.bounds.height - spacing) / 2)
+                            let rect3 = CGRectMake(rect2.origin.x , rect2.height + spacing, rect2.width , rect2.height)
                             
-                            self.loadImage(self.imageView1, file: imageFile)
+                            self.loadImage(self.imageView1, rect: rect1, file: self.getFile(photos, index: 0))
+                            self.loadImage(self.imageView2, rect: rect2, file: self.getFile(photos, index: 1))
+                            self.loadImage(self.imageView3, rect: rect3, file: self.getFile(photos, index: 2))
                             
-//                            self.loadImages(photos)
+                        case 4:
+                            let rect1 = CGRectMake(0.0, 0.0, self.bounds.width, self.bounds.height - spacing - ((self.bounds.width - (spacing * 2)) / 3))
+                            let rect2 = CGRectMake(0.0 , rect1.height + spacing, (self.bounds.width - (spacing * 2)) / 3, (self.bounds.width - (spacing * 2)) / 3)
+                            let rect3 = CGRectMake(rect2.width + spacing , rect2.origin.y, rect2.width, rect2.height)
+                            let rect4 = CGRectMake(rect3.origin.x + rect2.width + spacing , rect2.origin.y, rect2.width, rect2.height)
                             
-//                            let photo: PFObject = objects[0] as PFObject
-//                            
-//                            println("photo Description = \(photo[TablePhoto.Description])")
-//                            println("photo ImageWidth = \(photo[TablePhoto.ImageWidth])")
-//                            println("photo ImageHeight = \(photo[TablePhoto.ImageHeight])")
-//                            
-//                            let imageFile: PFFile = photo[TablePhoto.Image] as PFFile
-//                            
-//                            self.imageFile1 = imageFile
+                            self.loadImage(self.imageView1, rect: rect1, file: self.getFile(photos, index: 0))
+                            self.loadImage(self.imageView2, rect: rect2, file: self.getFile(photos, index: 1))
+                            self.loadImage(self.imageView3, rect: rect3, file: self.getFile(photos, index: 2))
+                            self.loadImage(self.imageView4, rect: rect4, file: self.getFile(photos, index: 3))
                             
-//                            self.imageView1.image = ImagePlaceholder.Image
-////                            if let theFile = imageFile {
-//                                imageFile.getDataInBackgroundWithBlock { (imageData, error) -> Void in
-//                                    if error == nil {
-//                                        if let image = UIImage(data: imageData) {
-//                                            self.imageView1.image = image
-//                                        }
-//                                    }
-//                                }
-////                            }
+                        case 5:
+                            let rect1 = CGRectMake(0.0, 0.0, self.bounds.width - ((self.bounds.height - (spacing * 2)) / 3) - spacing, (self.bounds.height - spacing) / 2)
+                            let rect2 = CGRectMake(0.0 , rect1.height + spacing, rect1.width , rect1.height)
+                            let rect3 = CGRectMake(rect1.width + spacing , 0, (self.bounds.height - (spacing * 2)) / 3, (self.bounds.height - (spacing * 2)) / 3)
+                            let rect4 = CGRectMake(rect3.origin.x, rect3.height + spacing, rect3.width , rect3.height)
+                            let rect5 = CGRectMake(rect3.origin.x, rect4.origin.y + rect3.height + spacing, rect3.width , rect3.height)
                             
-                            
-//                            self.loadImage(self.imageView1, file: imageFile)
-                            
-//                            file.getDataInBackgroundWithBlock({ (imageData, error) -> Void in
-//                                if error == nil {
-//                                    if let image = UIImage(data:imageData) {
-//                                        self.imageView1.image = image
-//                                    }
-//                                }
-//                            })
-//
-//                            let file: PFFile = objects[0][TablePhoto.Image] as PFFile
-//                            file.getDataInBackgroundWithBlock({ (imageData, error) -> Void in
-//                                if error == nil {
-//                                    if let image = UIImage(data:imageData) {
-//                                        self.imageView1.image = image
-//                                    }
-//                                }
-//                            })
-//                            
-//                            if let file: PFFile = objects[0][TablePhoto.Image] as? PFFile {
-//                                file.getDataInBackgroundWithBlock { (imageData, error) -> Void in
-//                                    if error == nil {
-//                                        if let image = UIImage(data:imageData) {
-//                                            self.imageView1.image = image
-//                                        }
-//                                    }
-//                                }
-//                            }
+                            self.loadImage(self.imageView1, rect: rect1, file: self.getFile(photos, index: 0))
+                            self.loadImage(self.imageView2, rect: rect2, file: self.getFile(photos, index: 1))
+                            self.loadImage(self.imageView3, rect: rect3, file: self.getFile(photos, index: 2))
+                            self.loadImage(self.imageView4, rect: rect4, file: self.getFile(photos, index: 3))
+                            self.loadImage(self.imageView5, rect: rect5, file: self.getFile(photos, index: 4))
                             
                         default:
                             break
@@ -143,7 +147,14 @@ class DuangTableCellImageMutable: UITableViewCell
         }
     }
     
-    func loadImage(imageView: UIImageView, file: PFFile?) {
+    func getFile(photos: [PFObject], index: NSInteger) -> PFFile {
+        let photo: PFObject = photos[index] as PFObject
+        return photo[TablePhoto.Image] as PFFile
+    }
+    
+    func loadImage(imageView: UIImageView, rect: CGRect, file: PFFile?) {
+        imageView.hidden = false
+        imageView.frame = rect
         imageView.image = ImagePlaceholder.Image
         if let theFile = file {
             theFile.getDataInBackgroundWithBlock { (imageData, error) -> Void in
@@ -155,49 +166,4 @@ class DuangTableCellImageMutable: UITableViewCell
             }
         }
     }
-    
-    
-/*
-    var duangTableDataRow: DuangTableDataRow? {
-        didSet {
-            if let row = duangTableDataRow {
-                
-//                if let image = row.getImageArray(0) {
-//                    cellImageView.image = image
-//                }
-                
-                
-                println("row.imageFileArray = \(row.imageFileArray)")
-                
-                if let imageFileArray = row.imageFileArray {
-                    switch imageFileArray.count {
-                    case 1:
-                        println("hello")
-                        
-//                    case 2:
-                        
-                    default:
-                        break
-                    }
-                }
-                
-                
-                
-//                swich row.imageFileArray.count {
-//                    case 1:
-//                    
-//                }
-                
-//                if let imageFile = row.getImageFileArray(0) {
-//                    imageFile.getDataInBackgroundWithBlock { (imageData, error) -> Void in
-//                        if error == nil {
-//                            if let image = UIImage(data:imageData) {
-//                                self.cellImageView.image = image
-//                            }
-//                        }
-//                    }
-//                }
-            }
-        }
-    }*/
 }
